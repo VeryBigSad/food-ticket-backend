8a48edbd05c01c775d0ece9a30326682d5439978
Mikhail Khromov <khromov05@gmail.com>
2022-10-16 21:53:07 +0300

    help command + registered some commands + created new app +

--- tgbot/handlers/onboarding/static_text.py
+++ tgbot/handlers/onboarding/static_text.py
@@ -1,6 +1,6 @@
 start_created = "Привет, друг! С помощью этого бота ты можешь получать завтраки и обеды в нашей столовой без " \
                 "талончиков! "
-start_not_created = ''
+start_not_created = start_created
 register_command_usage = 'ооо, новое мясо для ебли'
 register_successful = 'Ты успешно зарегистровался'
 register_failed = 'иди нахуй, с попущеными не общаюсь'
