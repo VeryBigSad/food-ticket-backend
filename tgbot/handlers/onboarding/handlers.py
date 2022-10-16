4dcb78659f0c7958173caf6e13a79b0f035489a3
Mikhail Khromov <khromov05@gmail.com>
2022-10-16 20:52:23 +0300

    сделал /start /register

--- tgbot/handlers/onboarding/handlers.py
+++ tgbot/handlers/onboarding/handlers.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext
 
+from food_tickets.models import Student
 from tgbot.handlers.onboarding import static_text
 from tgbot.handlers.utils.info import extract_user_data_from_update
 from users.models import TelegramUser
@@ -22,3 +23,24 @@ def command_start(update: Update, context: CallbackContext) -> None:
 
 def command_register(update: Update, context: CallbackContext) -> None:
     u, created = TelegramUser.get_user_and_created(update, context)
+
+    if len(update.message.text.split()) != 2:
+        update.message.reply_text(static_text.register_command_usage)
+        return
+    else:
+        code = update.message.text.split(' ')[1]
+        student_obj = Student.objects.filter(secret_code=code)
+        if student_obj.exists():
+            student_obj = student_obj[0]
+            text = static_text.register_successful.format(
+                full_name=student_obj.full_name,
+                date_of_birth=student_obj.date_of_birth
+            )
+            student_obj.telegram_account = u
+            student_obj.save()
+        else:
+            text = static_text.register_failed
+        update.message.reply_text(text)
+
+
+
--- tgbot/handlers/onboarding/static_text.py
+++ tgbot/handlers/onboarding/static_text.py
@@ -1,2 +1,5 @@
 start_created = "Sup, {first_name}!"
 start_not_created = "Welcome back, {first_name}!"
+register_command_usage = ''
+register_successful = '{full_name} {date_of_birth}'
+register_failed = ''
