af6e5b547e545368329f057ec37e21e5e29f791e
Mikhail Khromov <khromov05@gmail.com>
2022-11-01 18:02:32 +0300

    ehhhh hopefully works

--- food_tickets/forms.py
+++ food_tickets/forms.py
@@ -3,4 +3,3 @@ from django import forms
 
 class UploadExcelForm(forms.Form):
     file = forms.FileField(label="Файл excel с данными по питанию")
-
--- food_tickets/tasks.py
+++ food_tickets/tasks.py
@@ -1,6 +1,5 @@
 from dtb.celery import app
 from food_tickets.models import Student
-from food_tickets.utils import parse_excel_file
 
 
 @app.task(ignore_result=True)
@@ -8,4 +7,3 @@ def excel_update(student_list):
     Student.objects.all().update(has_food_right=False)
     for i in student_list:
         Student.objects.update_or_create(full_name=i[0], grade=i[1], defaults={'has_food_right': True})
-
